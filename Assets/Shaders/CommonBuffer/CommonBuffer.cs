7470ec62bd7ecff803b6ebb6d1c5ea3c9babac89
Halioli <aniolbarnadat@gmail.com>
2023-05-29 11:06:28 +0200

    Only missing additive pass to postProcess shader

--- Assets/Shaders/CommonBuffer/CommonBuffer.cs
+++ Assets/Shaders/CommonBuffer/CommonBuffer.cs
@@ -1,53 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Experimental.Rendering;
 using UnityEngine.Serialization;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
- 
+
 [ExecuteInEditMode]
-public class MyCommandBuffer : MonoBehaviour
+public class CommonBuffer : MonoBehaviour
 {
-    [ColorUsageAttribute(true, true)] 
-        public Color rimlightColor = Color.white;
-    public float blurSize = 0.05f;
- 
-     public Shader shader;
- 
+    [ColorUsageAttribute(true, true)]
+    public Color rimlightColor = Color.white;
+    public float blurSize = 1000f;
+    public int mipLvl = 4;
+
+    public Shader shader;
+
     // list of all renderer components you want to have outlined as a single silhouette
     public Renderer[] renderers;
-    const string shaderName = "Unlit/SHDR_PostProcess";
- 
+    const string shaderName = "PP/MyPostProcess";
+
     // shader pass indices
     private const int SHADER_PASS_MASK = 0;
     private const int SHADER_PASS_BLUR = 1;
- 
+    private const int SHADER_PASS_ADDITIVE = 2;
+
     // render texture IDs
     private int maskBuffer = Shader.PropertyToID("_Mask");
     private int glowBuffer = Shader.PropertyToID("_Glow");
- 
- 
- 
+    private int mainBuffer = Shader.PropertyToID("_Main");
+
+
+
     // private variables
     private UnityEngine.Rendering.CommandBuffer cb;
     private Material mat;
     private Camera cam;
     public CameraEvent cameraEvent = CameraEvent.BeforeForwardOpaque;
- 
+
     private Mesh MeshFromRenderer(Renderer r)
     {
         if (r is MeshRenderer)
             return r.GetComponent<MeshFilter>().sharedMesh;
- 
+
         return null;
     }
- 
+
     private void CreateCommandBuffer(Camera cam)
     {
         if (renderers == null || renderers.Length == 0)
             return;
- 
+
         if (cb == null)
         {
             cb = new UnityEngine.Rendering.CommandBuffer();
@@ -57,28 +63,28 @@ public class MyCommandBuffer : MonoBehaviour
         {
             cb.Clear();
         }
- 
+
         if (mat == null)
         {
             mat = new Material(shader != null ? shader : Shader.Find(shaderName));
         }
- 
+
         // do nothing if no rimlight will be visible
-        if (rimlightColor.a <= (1f/255f) || blurSize <= 0f)
+        if (rimlightColor.a <= (1f / 255f) || blurSize <= 0f)
         {
             cb.Clear();
             return;
         }
- 
+
         // support meshes with sub meshes
         // can be from having multiple materials, complex skinning rigs, or a lot of vertices
         int renderersCount = renderers.Length;
         int[] subMeshCount = new int[renderersCount];
- 
-        for (int i=0; i<renderersCount; i++)
+
+        for (int i = 0; i < renderersCount; i++)
         {
             var mesh = MeshFromRenderer(renderers[i]);
- 
+
             if (mesh != null)
             {
                 // assume staticly batched meshes only have one sub mesh
@@ -88,53 +94,83 @@ public class MyCommandBuffer : MonoBehaviour
                     subMeshCount[i] = mesh.subMeshCount;
             }
         }
- 
+
         // match current quality settings' MSAA settings
         // doesn't check if current camera has MSAA enabled
         // also could just always do MSAA if you so pleased
         int msaa = 1;
- 
+
         int width = cam.scaledPixelWidth;
         int height = cam.scaledPixelHeight;
- 
- 
+
+
         // setup descriptor for descriptor of inverted alpha render texture
-        RenderTextureDescriptor MaskRTD = new RenderTextureDescriptor() 
+        RenderTextureDescriptor MaskRTD = new RenderTextureDescriptor()
         {
             dimension = TextureDimension.Tex2D,
             graphicsFormat = GraphicsFormat.A10R10G10B10_XRUNormPack32,
- 
+
             width = width,
             height = height,
- 
+
             msaaSamples = msaa,
             depthBufferBits = 0,
- 
+
             sRGB = false,
- 
+
             useMipMap = true,
             autoGenerateMips = true
         };
- 
+
         cb.GetTemporaryRT(maskBuffer, MaskRTD, FilterMode.Trilinear);
- 
+
         // render meshes to main buffer for the interior stencil mask
         cb.SetRenderTarget(maskBuffer);
         cb.ClearRenderTarget(true, true, Color.clear);
-        for (int i=0; i<renderersCount; i++)
+        for (int i = 0; i < renderersCount; i++)
         {
-            for(int m = 0; m < subMeshCount[i]; m++)
+            for (int m = 0; m < subMeshCount[i]; m++)
             {
                 cb.DrawRenderer(renderers[i], mat, m, SHADER_PASS_MASK);
- 
+
             }
         }
- 
-        cb.SetRenderTarget(BuiltinRenderTextureType.CameraTarget);
- 
- 
+
+        //cb.SetRenderTarget(BuiltinRenderTextureType.CameraTarget);
+
+        // setup descriptor for descriptor of inverted alpha render texture
+        RenderTextureDescriptor GlowRTD = new RenderTextureDescriptor()
+        {
+            dimension = TextureDimension.Tex2D,
+            graphicsFormat = GraphicsFormat.A10R10G10B10_XRUNormPack32,
+
+            width = width,
+            height = height,
+
+            msaaSamples = msaa,
+            depthBufferBits = 0,
+
+            sRGB = false,
+
+            useMipMap = true,
+            autoGenerateMips = true
+        };
+
+        mat.SetFloat("_Distance", blurSize);
+        mat.SetFloat("_MipLVL", mipLvl);
+
+        // create silhouette buffer and assign it as the current render target
+        cb.GetTemporaryRT(glowBuffer, GlowRTD, FilterMode.Trilinear);
+        cb.Blit(maskBuffer, glowBuffer, mat, SHADER_PASS_BLUR);
+
+        //mat.SetTexture("_Main", );
+        cb.Blit(mainBuffer, BuiltinRenderTextureType.CameraTarget, mat, SHADER_PASS_ADDITIVE);
+
+        cb.ReleaseTemporaryRT(maskBuffer);
+        cb.ReleaseTemporaryRT(glowBuffer);
+        cb.ReleaseTemporaryRT(mainBuffer);
     }
- 
+
     void RemoveCommandBuffer(Camera cam)
     {
         if (this.cam != null && cb != null)
@@ -143,37 +179,36 @@ public class MyCommandBuffer : MonoBehaviour
             this.cam = null;
         }
     }
- 
+
     void ApplyCommandBuffer(Camera cam)
     {
         CreateCommandBuffer(cam);
-
         if (cb == null)
             return;
- 
+
         this.cam = cam;
         this.cam.AddCommandBuffer(cameraEvent, cb);
     }
- 
+
     void OnEnable()
     {
         Camera.onPreRender += ApplyCommandBuffer;
         Camera.onPostRender += RemoveCommandBuffer;
     }
- 
+
     void OnDisable()
     {
         Camera.onPreRender -= ApplyCommandBuffer;
         Camera.onPostRender -= RemoveCommandBuffer;
     }
- 
+
 #if UNITY_EDITOR
     void OnValidate()
     {
         if (shader == null)
             shader = Shader.Find(shaderName);
     }
- 
- 
+
+
 #endif
 }
--- Assets/Shaders/CommonBuffer/SHDR_PostProcess.shader
+++ Assets/Shaders/CommonBuffer/SHDR_PostProcess.shader
@@ -10,9 +10,10 @@ Shader "Unlit/SHDR_PostProcess"
         Tags { "RenderType"="Opaque" }
         LOD 100
 
-        /// PASS 0 - Invert color
+        // Pass 0 - Invert Color
         Pass
         {
+            Name "Mask"
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
@@ -31,7 +32,7 @@ Shader "Unlit/SHDR_PostProcess"
                 float4 vertex : SV_POSITION;
             };
 
-            sampler2D _MainTex, _PrePass;
+            sampler2D _MainTex;
             float4 _MainTex_ST;
 
             v2f vert (appdata v)
@@ -43,10 +44,117 @@ Shader "Unlit/SHDR_PostProcess"
             }
 
             fixed4 frag (v2f i) : SV_Target
+            {                
+                return fixed4(1,0, 0,1);
+            }
+            ENDCG
+        }
+
+        // Pass 1 - Glow
+        Pass
+        {
+            Name "Glow"
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
             {
-                // sample the texture
-                fixed4 col = tex2D(_MainTex, i.uv);
-                return 1 - col;
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex, _Glow;
+            float4 _MainTex_ST, _MainTex_TexelSize;
+            float _Distance, _MipLVL;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 Blur(sampler2D textureToBlur, float4 textureTexelSize, float2 uv, float distance)
+            {
+                fixed4 sum = fixed4(0.0, 0.0, 0.0, 0.0);
+
+                sum += tex2Dlod(textureToBlur, half4(uv.x, uv.y - 4.0 * 0.05 * distance * textureTexelSize.y, 0, _MipLVL)) * 0.05;
+				sum += tex2Dlod(textureToBlur, half4(uv.x, uv.y - 3.0 * 0.05 * distance * textureTexelSize.y, 0, _MipLVL)) * 0.09;
+				sum += tex2Dlod(textureToBlur, half4(uv.x, uv.y - 2.0 * 0.05 * distance * textureTexelSize.y, 0, _MipLVL)) * 0.12;
+				sum += tex2Dlod(textureToBlur, half4(uv.x, uv.y, 0, 6)) * 0.16;
+				sum += tex2Dlod(textureToBlur, half4(uv.x, uv.y + 2.0 * 0.05 * distance * textureTexelSize.y, 0, _MipLVL)) * 0.12;
+				sum += tex2Dlod(textureToBlur, half4(uv.x, uv.y + 3.0 * 0.05 * distance * textureTexelSize.y, 0, _MipLVL)) * 0.09;
+				sum += tex2Dlod(textureToBlur, half4(uv.x, uv.y + 4.0 * 0.05 * distance * textureTexelSize.y, 0, _MipLVL)) * 0.05;
+ 
+                sum += tex2Dlod(textureToBlur, half4(uv.x - 4.0 * 0.05 * distance  * textureTexelSize.x, uv.y, 0, _MipLVL)) * 0.05;
+				sum += tex2Dlod(textureToBlur, half4(uv.x - 3.0 * 0.05 * distance * textureTexelSize.x, uv.y, 0, _MipLVL)) * 0.09;
+				sum += tex2Dlod(textureToBlur, half4(uv.x - 2.0 * 0.05 * distance * textureTexelSize.x, uv.y, 0, _MipLVL)) * 0.12;
+				sum += tex2Dlod(textureToBlur, half4(uv.x, uv.y, 0, 6)) * 0.16;
+				sum += tex2Dlod(textureToBlur, half4(uv.x + 2.0 * 0.05 * distance * textureTexelSize.x, uv.y, 0, _MipLVL)) * 0.12;
+				sum += tex2Dlod(textureToBlur, half4(uv.x + 3.0 * 0.05 * distance * textureTexelSize.x, uv.y, 0, _MipLVL)) * 0.09;
+				sum += tex2Dlod(textureToBlur, half4(uv.x + 4.0 * 0.05 * distance * textureTexelSize.x, uv.y, 0, _MipLVL)) * 0.05;
+
+                return sum;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {                
+                fixed4 color = tex2Dlod(_MainTex, float4(i.uv, 0, 0));
+                
+                fixed4 blur = Blur(_MainTex, _MainTex_TexelSize, i.uv, _Distance);              
+
+                return saturate(blur) - color;                
+            }
+            ENDCG
+        }
+
+        // Pass 2 - Additive
+        Pass
+        {
+            Name "Additive"
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex, _Glow, _Main;
+            float4 _MainTex_ST, _MainTex_TexelSize;
+            float _Distance;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {                
+                return tex2D(_MainTex, i.uv);
             }
             ENDCG
         }
